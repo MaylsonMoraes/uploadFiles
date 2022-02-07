a2a6c14bd0c2e16dfd62313e526f4b9ecfd5242f
Maylson <maylsonmoraesd@hotmail.com>
2022-02-07 16:08:31 -0300

    correção erro

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const storage = multer.diskStorage({
         cb(null,"uploads/")
     },
     filename: function(req, file, cb){
-        cb(null, file.originalname + Date.now() + path.extname(file.));
+        cb(null, file.originalname + Date.now() + path.extname(file.originalname));
     }
 })
 
